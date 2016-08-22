caa0c5f6b30a31cfe3d5ab4088000c4a378b04cf
Manobhav Jain <manobhav.jain@gmail.com>
2016-08-22 14:31:08 +0530

    master commit

--- app/src/main/java/com/example/manobhavjain/projectkasm/card_activityEdit.java
+++ app/src/main/java/com/example/manobhavjain/projectkasm/card_activityEdit.java
@@ -48,8 +48,8 @@ public class card_activityEdit extends AppCompatActivity {
     private AdapterClass manuAdapter;
     private ArrayList<Data> myObjects;
 
-    private static final int REQUEST_PHOTO=1;
-    private static final int REQUEST_GALLERY=2;
+    private static final int REQUEST_PHOTO=2;
+    private static final int REQUEST_GALLERY=1;
     private Cardbase cardbase;
 
     public static Intent newInstanceEmpty(Context context, UUID uuid){
